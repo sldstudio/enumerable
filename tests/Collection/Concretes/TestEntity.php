0e458df9ea602d358300947f0f98e28e870a8d91
Ruslan <fisher7410@yandex.ru>
2023-03-31 01:09:53 +0500

    Added generic collection

---
+++ tests/Collection/Concretes/TestEntity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Collection\Concretes;
+
+/**
+ * @internal
+ */
+final class TestEntity
+{
+    protected string $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
